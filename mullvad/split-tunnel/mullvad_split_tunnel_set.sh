528910640587e4b443a10dcda5ad5dfba4584912
it4world <itgg2@proton.me>
2024-06-11 16:58:11 +0200

    Decreased frequency

--- mullvad/split-tunnel/mullvad_split_tunnel_set.sh
+++ mullvad/split-tunnel/mullvad_split_tunnel_set.sh
@@ -4,8 +4,8 @@ sync_status=$(timedatectl status | grep -oP 'System clock synchronized: \K\w+')
 
 if [[ $sync_status == "no" ]]; then
 
-    # system time is not synchronized - set split tunnel for systemd-timesyncd service
-	
+	# system time is not synchronized - set split tunnel for systemd-timesyncd service
+
 	# Get PID of systemd-timesyncd
 	SERVICE_PID=$(pgrep -f systemd-timesyncd)
 
@@ -13,12 +13,12 @@ if [[ $sync_status == "no" ]]; then
 		# Add PID to Mullvad split tunnel configuration
 		mullvad split-tunnel add $SERVICE_PID
 		echo "New VPN Split tunnel was set for PID $SERVICE_PID"
-        exit 0
+		exit 0
 	else
 		echo "Failed to get PID of systemd-timesyncd."
-        exit 1
+		exit 1
 	fi
 else
-    echo "System time is synchronized, no need of setting VPN split tunnel"
-    exit 0
+	echo "System time is synchronized, no need of setting VPN split tunnel"
+	exit 0
 fi
