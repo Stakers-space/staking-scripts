20d96fe9511f8e97996e98e1d1c31a46a2b914ed
it4world <itgg2@proton.me>
2024-07-31 10:51:49 +0200

    Test

--- monitor/validators_state/validators_state_checker.js
+++ monitor/validators_state/validators_state_checker.js
@@ -1,4 +1,4 @@
-// Version 1.0.3
+// Version 1.0.4
 const pubKeysList = require("./public_keys_testlist.json");
 const beaconClientUrl = "http://localhost:9596/eth/v1/beacon";
 
@@ -10,6 +10,7 @@ console.log("pubKeys_instances:", pubKeys_instances);
 
 const http = require('http');
 const startTime = new Date().getTime();
+
 
 // Get slot from Headers data
 GetBeaconApiData("/headers", function(err,resp){
@@ -56,6 +57,7 @@ function GetPubKeyStateData(instanceIndex, pubkeyIndex, cb){ // synchronously in
         console.log(`GetPubKeyStateData iteration | pubkey ${pubkeyIndex}/${instanceData.count} in instance ${instanceIndex}/${pubKeys_instances.length} | ${instancePubKey}`);
         // Compare to public_keys_list
             // prepare aggregated state (if everything ok, then "OK" only)
+            console.log("validator resp:", resp);
 
         // continue on the next pubkey
         pubkeyIndex++;
@@ -73,6 +75,7 @@ function GetPubKeyStateData(instanceIndex, pubkeyIndex, cb){ // synchronously in
         }
     });
 }
+
 
 function GetBeaconApiData(path, cb){
     http.get(beaconClientUrl+path, (resp) => {
@@ -97,3 +100,76 @@ function cleanUpAndExit() {
 }
 process.on('SIGTERM', cleanUpAndExit);
 process.on('SIGINT', cleanUpAndExit);
+
+
+// -------------------------------------------- Test 2
+const lodestarApiUrl = 'http://localhost:9596/eth/v1';
+function httpGet(url) {
+    return new Promise((resolve, reject) => {
+        http.get(url, (res) => {
+            let data = '';
+
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                resolve(JSON.parse(data));
+            });
+        }).on('error', (err) => {
+            reject(err);
+        });
+    });
+}
+
+async function getValidatorInfo(pubKey) {
+    try {
+        const url = `${lodestarApiUrl}/beacon/states/head/validators?id=${pubKey}`;
+        const response = await httpGet(url);
+        return response.data[0];
+    } catch (error) {
+        console.error('Error fetching validator info:', error);
+        throw error;
+    }
+}
+
+async function getBlockAttestations(slot) {
+    try {
+        const url = `${lodestarApiUrl}/beacon/blocks/${slot}/attestations`;
+        const response = await httpGet(url);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching block attestations:', error);
+        throw error;
+    }
+}
+
+async function getLastAttestation(pubKey) {
+    try {
+        const validatorInfo = await getValidatorInfo(pubKey);
+        const lastAttestationSlot = validatorInfo.index;
+
+        const attestations = await getBlockAttestations(lastAttestationSlot);
+        const validatorAttestation = attestations.find(attestation =>
+            attestation.data.attesting_indices.includes(validatorInfo.validator.index)
+        );
+
+        return {
+            pubKey,
+            lastAttestation: validatorAttestation ? validatorAttestation.data : null
+        };
+    } catch (error) {
+        console.error('Error fetching last attestation:', error);
+        throw error;
+    }
+}
+
+(async () => {
+    const pubKeys = [
+        '0xa1d1ad0714035353258038e964ae9675dc0252ee22cea896825c01458e1807bfad2f9969338798548d9858a571f7425c'
+        // Next pubkey
+    ];
+
+    const validatorAttestations = await Promise.all(pubKeys.map(getLastAttestation));
+    console.log(validatorAttestations);
+})();
