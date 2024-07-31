1ad74e2547db25af33802b70eead0466a62327ef
it4world <itgg2@proton.me>
2024-07-31 19:26:48 +0200

    Validators state monitor - test release

--- monitor/validators_state/utils/generate-public-keys-list-from-deposit_files.js
+++ monitor/validators_state/utils/generate-public-keys-list-from-deposit_files.js
@@ -4,33 +4,86 @@ const GetFilesContent = require('../../../utils/get_files_content/get-files-data
 
 const publicKeysList_outputFile = path.join(__dirname, '..', 'public_keys_list.json');
 const depositsDataFileDirectory = path.join(__dirname, '..', '..', '..', 'get-validator-indexes/data/deposit_data');
-console.log("depositsData", depositsDataFileDirectory, "publicKeys:", publicKeysList_outputFile);
+const offlinePreparationFilePath = path.join(__dirname, '..', '..', '..', 'get-validator-indexes/data/offline-preparation.json');
+console.log("depositsData", depositsDataFileDirectory, "\nofflinePreparationFilePath:", offlinePreparationFilePath, "\npublicKeys:", publicKeysList_outputFile);
+
+class ValidatorDataModel {
+    constructor(count, validatorIndexes){
+        this.c = count;
+        this.v = validatorIndexes;
+    }
+
+    findDuplicateIndexes(data) {
+        const keyCount = {};
+    
+        Object.values(data).forEach(item => {
+            item.v.forEach(key => {
+                keyCount[key] = (keyCount[key] || 0) + 1;
+            });
+        });
+    
+        const duplicates = Object.keys(keyCount).filter(key => keyCount[key] > 1);
+        return duplicates;
+    }
+}
+
+var callbacks = 2;
+var offlinePreparationData = {};
+var validatorsData = null;
+var skippedPubKeys = {};
+
+function ProcessData(err){
+    if(err){
+        console.error(err);
+        return;
+    }
+    callbacks--;
+    if(callbacks > 0) return;
+
+    const publicKeys = GetPublicKeys(validatorsData, offlinePreparationData);
+    // duplicity check
+    const duplicatedKeys = new ValidatorDataModel().findDuplicateIndexes(publicKeys);
+    if(duplicatedKeys.length > 0) {
+        console.error("X Found "+duplicatedKeys.length+" duplicities in deposit files. Duplicated pubkeys:", duplicatedKeys);
+        return;
+    }
+    console.log(`✓ Duplicity check: ${duplicatedKeys.length} found duplicities`)
+
+    UpdatePublicKeysListFile(publicKeys, function(err){
+        if(err){
+            console.error(err);
+        } else {
+            console.log(`${publicKeysList_outputFile} file has been updated`);
+        }
+    });
+}
+
+// load offline data file (contain indices)
+fs.readFile(offlinePreparationFilePath, 'utf8', (err, data) => {
+    if (!err) {
+        var opd = JSON.parse(data).validators,
+        offlinePreparationData_src_l = opd.length;
+        //console.log(offlinePreparationData_src_l, opd);
+        for(var i=0;i<offlinePreparationData_src_l;i++){
+            //console.log(opd[i].pubkey, opd[i].index);
+            offlinePreparationData[opd[i].pubkey] = opd[i].index; // attach index for each pubkey
+        }
+    }
+    ProcessData(err);
+});
 
 GetFilesContent(depositsDataFileDirectory, 'deposit_data-', '.json', (err, content) => {
     if (err) {
         console.error('Error:', err);
     } else {
-        console.log('Files Content | keys:', Object.keys(content));
-        const publicKeys = GetPublicKeys(content);
-
-        // duplicity check
-        const duplicatedKeys = findDuplicateKeys(publicKeys);
-        if(duplicatedKeys.length > 0) {
-            console.error("Found "+duplicatedKeys.length+" duplicities in deposit files. Duplicated pubkeys:", duplicatedKeys);
-            return;
-        }
-
-        UpdatePublicKeysListFile(GetPublicKeys(content), function(err){
-            if(err){
-                console.error(err);
-            } else {
-                console.log(`${publicKeysList_outputFile} file has been updated`);
-            }
-        });
+       // console.log('Files Content | keys:', Object.keys(content));
+        validatorsData = content;
+        ProcessData(err);
     }
 });
 
-function GetPublicKeys(depositDataFiles){
+function GetPublicKeys(depositDataFiles, offlinePreparationData){
+    //console.log(offlinePreparationData);
     let pubKeys = {};
     for (const ddfile in depositDataFiles) {
         if (depositDataFiles.hasOwnProperty(ddfile)) {
@@ -42,12 +95,26 @@ function GetPublicKeys(depositDataFiles){
                 if(validatorsL === 0) continue;
                 let instanceKeys = [];
                 for (var y=0;y<validatorsL;y++){
-                    instanceKeys.push(String("0x"+value[y].pubkey));
+                    //instanceKeys.push(String("0x"+value[y].pubkey));
+                    const pubKeyIndex = offlinePreparationData[String("0x"+value[y].pubkey)];
+                    if(pubKeyIndex){
+                        //console.log(String("0x"+value[y].pubkey),"=>",pubKeyIndex);
+                        instanceKeys.push(Number(pubKeyIndex));
+                    } else {
+                        //console.log(`Warn | ${value[y].pubkey} from ${ddfile} has not been activated yet. Skipping...`);
+                        if(!skippedPubKeys[ddfile]) skippedPubKeys[ddfile] = 0;
+                        skippedPubKeys[ddfile]++;
+                    }
                 }
-                pubKeys[ddfile] = {
-                    count: instanceKeys.length,
-                    pubKeys: instanceKeys
-                };
+                var instanceIdentifier = ddfile;
+                // remove ending
+                if(instanceIdentifier.indexOf(".") > -1) instanceIdentifier = instanceIdentifier.replace(/\.json$/, "");
+                // Use only part after last `_`
+                if(instanceIdentifier.indexOf("_") > -1){
+                    instanceIdentifier = instanceIdentifier.split("_");
+                    instanceIdentifier = instanceIdentifier[instanceIdentifier.length-1];
+                }
+                pubKeys[instanceIdentifier] = new ValidatorDataModel(instanceKeys.length, instanceKeys);
             } else {
                 console.log(`DDFile: ${ddfile}, is not an array`);
             }
@@ -62,17 +129,4 @@ function UpdatePublicKeysListFile(data, cb){
     fs.writeFile(publicKeysList_outputFile, fileContent, (err) => {
         return cb(err);
     });
-}
-
-function findDuplicateKeys(data) {
-    const keyCount = {};
-
-    Object.values(data).forEach(item => {
-        item.pubKeys.forEach(key => {
-            keyCount[key] = (keyCount[key] || 0) + 1;
-        });
-    });
-
-    const duplicates = Object.keys(keyCount).filter(key => keyCount[key] > 1);
-    return duplicates;
 }
--- monitor/validators_state/validators_state_checker.js
+++ monitor/validators_state/validators_state_checker.js
@@ -1,140 +1,153 @@
-// Version 1.0.7
+// Version 1.0.8
 const pubKeysList = require("./public_keys_testlist.json");
-const beaconClientUrl = "http://localhost:9596/eth/v1/beacon";
+const beaconChainPort = 9596;
 
 // script variables and resources
 const pubKeys_instances = Object.keys(pubKeysList);
-var instanceIndex = 0;
-var pubKeyIndex = 0;
 console.log("pubKeys_instances:", pubKeys_instances);
 
 const http = require('http');
-const startTime = new Date().getTime();
+var app = null;
 
-
-// Get slot from Headers data
-/*GetBeaconApiData("/headers", function(err,resp){
-    if(err) {
-        console.error(err);
-        return;
+class MonitorValidators {
+    constructor(){
+        this.indexesBanch = 100;
+        this.isRunning = false;
+        this.aggregatedStates = null;
+        app = this;
     }
 
-    const slot = resp["data"][0].header.message.slot;
-    //console.log("headers data:", resp["data"], "slot:", slot);
+    CronWorker(){
+        //this.cron = setInterval(function(){
+            if(!this.isRunning) this.PromptManagerScript();
+        //}, 60000);
+    }
 
-    // Get Attestations data for last slot
-    GetBeaconApiData("/blocks/"+slot+"/attestations", function(err,resp){
-        if(err) {
-            console.error(err);
-            return;
-        }
+    PromptManagerScript(){
+        this.isRunning = true;
+        this.aggregatedStates = {};
+        this.startTime = new Date().getTime();
+        console.log(`${this.startTime} Starting MonitorValidators iteration`);
+        this.GetLatestBlockNumber(function(err,resp){            
+            // parse data
+            try { resp = JSON.parse(resp); } catch(e){ err = e; }
+            if(err) {
+                console.log(err);
+                return;
+            }
+            
+            console.log(resp);
+            console.log(resp["data"][0]);
+            const blockNumber = resp["data"][0].header.message.slot; // replace to block
+            console.log(`├─ latest block: ${blockNumber}`);
+            // Process Check
+            app.ProcessCheck(0,0, blockNumber, function(err){
+                if(err) {
+                   console.error(err); 
+                   return;
+                }
+                const now = new Date().getTime();
+                const totalProcessingTime = now - app.startTime;
+                console.log(`${now} MonitorValidators iteration completed in ${totalProcessingTime}`);
 
-        const attestationsData = resp["data"];
-        console.log(resp);
-        console.log(`attestations data for slot ${slot} | commiteeIndexes: ${attestationsData.length}`);
-        for (var i=0;i<attestationsData.length;i++){
-            console.log(attestationsData[i]);
-        }
-
-        GetPubKeyStateData(instanceIndex, pubKeyIndex, function(err,resp){
-            //console.log(err,resp);
-            // process aggregated state update
-            console.log("Monitor state completed in", new Date().getTime() - startTime);
+                console.log("Posting aggregated data");
+                app.isRunning = false;
+            });
         });
-    });
-});*/
+    }
 
+    ProcessCheck(instanceIndex, pubKeyStartIndex, blockNumber, cb){
+        const instanceData = pubKeysList[pubKeys_instances[instanceIndex]];
+        const instancePubKeys = instanceData.v;
 
-function GetPubKeyStateData(instanceIndex, pubkeyIndex, cb){ // synchronously in a single thread - what's the time of the whole iteration? Split it into more threads?
-    const instanceData = pubKeysList[pubKeys_instances[instanceIndex]];
-    const instancePubKey = pubKeysList[pubKeys_instances[instanceIndex]].pubKeys[pubkeyIndex];
-    
-    // Get data from beacon api
-    GetBeaconApiData("/states/head/validators?id="+instancePubKey, function(err,resp){
-        if(err) {
-            return cb(err, {"instanceIndex":instanceIndex,"pubKeyIndex":pubKeyIndex, "pubKey": instanceData.pubKeys[pubKeyIndex]});
-        }
-        console.log(`GetPubKeyStateData iteration | pubkey ${pubkeyIndex}/${instanceData.count} in instance ${instanceIndex}/${pubKeys_instances.length} | ${instancePubKey}`);
-        // Compare to public_keys_list
-            // prepare aggregated state (if everything ok, then "OK" only)
-            console.log("validator resp:", resp, resp.data[0].validator);
+        const indexesNumToRequest = (pubKeyStartIndex + this.indexesBanch <= instanceData.c) ? ((this.indexesBanch <= instanceData.c) ? this.indexesBanch : instanceData.c) : (instanceData.c - pubKeyStartIndex);
+        const endIndex = pubKeyStartIndex + indexesNumToRequest;
+        const validatorIndexes = instancePubKeys.slice(pubKeyStartIndex, endIndex);
 
-        // continue on the next pubkey
-        pubkeyIndex++;
-        console.log(`pubKey increased to ${pubkeyIndex} | pubkeyIndex === instanceData.count || ${pubkeyIndex} === ${instanceData.count} =>`, (pubkeyIndex === instanceData.count));
-        if(pubkeyIndex === instanceData.count) {
-            instanceIndex++
-            pubkeyIndex = 0;
-            console.log(`instanceIndex increased to ${instanceIndex} | pubKey reseted to ${pubkeyIndex}`);
-        }
-        console.log(`compare | instanceIndex === pubKeys_instances.length || ${instanceIndex} === ${pubKeys_instances.length} =>`, (instanceIndex === pubKeys_instances.length));
-        if(instanceIndex === pubKeys_instances.length) {
-            return cb();
-        } else {
-            GetPubKeyStateData(instanceIndex, pubkeyIndex, cb);
-        }
-    });
-}
+        // Get data from beacon api
+        this.GetValidatorLivenessState(validatorIndexes, blockNumber, function(err,resp){
+             // parse data
+             try { resp = JSON.parse(resp); } catch(e){ err = e; }
+             if(err) {
+                return cb(err, {"instanceIndex":instanceIndex,"pubKeyIndex":pubKeyIndex, "pubKey": instanceData.pubKeys[pubKeyIndex]});
+            }
 
+            // processResponse aggregation
+            console.log(resp);
+            // app.aggregatedStates
 
-function GetBeaconApiData(path, cb){
-    http.get(beaconClientUrl+path, (resp) => {
-        let data = '';
-        resp.on('data', (chunk) => {
-            data += chunk;
+            pubKeyStartIndex += app.indexesBanch;
+            //console.log(`pubKeyStartIndex increased to ${pubKeyStartIndex} | endIndex === instanceData.c || ${endIndex} === ${instanceData.c} =>`, (endIndex === instanceData.c));
+            if(endIndex === instanceData.c) {
+                 instanceIndex++
+                 pubKeyStartIndex = 0;
+                 console.log(`instanceIndex increased to ${instanceIndex} | pubKeyStartIndex reseted to ${pubKeyStartIndex}`);
+            }
+            //console.log(`compare | instanceIndex === pubKeys_instances.length || ${instanceIndex} === ${pubKeys_instances.length} =>`, (instanceIndex === pubKeys_instances.length));
+            if(instanceIndex === pubKeys_instances.length) {
+                 return cb();
+            } else {
+                app.ProcessCheck(instanceIndex, pubKeyStartIndex, blockNumber, cb);
+            }
         });
-      
-        resp.on('end', () => {
-            return cb(null, JSON.parse(data));
+    }
+
+    HttpRequest(options, body, cb){
+        const req = http.request(options, (res) => {
+            let response = '';
+            res.on('data', (chunk) => { response += chunk; });
+            res.on('end', () => { return cb(null, response); });     
+        }).on('error', (err) => {
+            cb(err);
         });
-      
-    }).on("error", (err) => {
-        return cb(err, null);
-    });
-}
+        if(body) req.write(body);
+        req.end();
+    }
 
-async function getValidatorLiveness() {
-    try {
-        const body = JSON.stringify(["1","2","3","4","5"]);
-
+    GetLatestBlockNumber(cb){
         const options = {
             hostname: 'localhost',
-            port: 9596,
-            path: '/eth/v1/validator/liveness/1042840', // block 1042840
+            port: beaconChainPort,
+            path: `/eth/v1/beacon/headers`,
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+            }
+        }
+        this.HttpRequest(options, null, cb);
+    }
+
+    GetValidatorLivenessState(validatorIndexes, blockNumber, cb) {
+        const body = JSON.stringify(validatorIndexes);
+        const options = {
+            hostname: 'localhost',
+            port: beaconChainPort,
+            path: `/eth/v1/validator/liveness/${blockNumber}`, // block 1042840
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 'Content-Length': body.length
             }
         };
+        this.HttpRequest(options, body, cb);
+    }
 
-        return new Promise((resolve, reject) => {
-            const req = http.request(options, (res) => {
-                let data = '';
-
-                res.on('data', (chunk) => {
-                    data += chunk;
-                });
-
-                res.on('end', () => {
-                    resolve(JSON.parse(data));
-                });
-            }).on('error', (err) => {
-                reject(err);
-            });
-            req.write(body);
-            req.end();
-        });
-    } catch (error) {
-        console.error('Error fetching validator liveness:', error);
-        throw error;
+    Exit(){
+        clearInterval(cronInterval);
     }
 }
 
-(async () => {
-    const validatorLiveness = await getValidatorLiveness();
-    console.log(validatorLiveness);
-})();
+// each 60 seconds = 1 block
+new MonitorValidators().CronWorker();
+
+// Testing
+/*app.ProcessCheck(0,0, 0, function(err){
+    if(err) {
+       console.error(err); 
+       return;
+    }
+    console.log(`${now} MonitorValidators test iteration completed`);
+});*/
 
 function cleanUpAndExit() {
     console.log("exiting");
