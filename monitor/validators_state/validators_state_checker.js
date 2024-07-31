595165a6a95a459b908b53bf0d295d951040be79
it4world <itgg2@proton.me>
2024-07-31 11:46:11 +0200

    Liveness

--- monitor/validators_state/validators_state_checker.js
+++ monitor/validators_state/validators_state_checker.js
@@ -1,4 +1,4 @@
-// Version 1.0.5
+// Version 1.0.6
 const pubKeysList = require("./public_keys_testlist.json");
 const beaconClientUrl = "http://localhost:9596/eth/v1/beacon";
 
@@ -13,7 +13,7 @@ const startTime = new Date().getTime();
 
 
 // Get slot from Headers data
-GetBeaconApiData("/headers", function(err,resp){
+/*GetBeaconApiData("/headers", function(err,resp){
     if(err) {
         console.error(err);
         return;
@@ -42,7 +42,7 @@ GetBeaconApiData("/headers", function(err,resp){
             console.log("Monitor state completed in", new Date().getTime() - startTime);
         });
     });
-});
+});*/
 
 
 function GetPubKeyStateData(instanceIndex, pubkeyIndex, cb){ // synchronously in a single thread - what's the time of the whole iteration? Split it into more threads?
@@ -93,76 +93,43 @@ function GetBeaconApiData(path, cb){
     });
 }
 
-function cleanUpAndExit() {
-    console.log("exiting");
-    // exit services
-    process.exit(0);
-}
-process.on('SIGTERM', cleanUpAndExit);
-process.on('SIGINT', cleanUpAndExit);
-
-
-// -------------------------------------------- Test 2
-const lodestarApiUrl = 'http://localhost:9596/eth/v1';
-function httpGet(url) {
-    return new Promise((resolve, reject) => {
-        http.get(url, (res) => {
-            let data = '';
-
-            res.on('data', (chunk) => {
-                data += chunk;
-            });
-
-            res.on('end', () => {
-                resolve(JSON.parse(data));
-            });
-        }).on('error', (err) => {
-            reject(err);
-        });
-    });
-}
-
-async function getValidatorInfo(pubKey) {
+async function getValidatorLiveness(pubKeys) {
     try {
-        const url = `${lodestarApiUrl}/beacon/states/head/validators?id=${pubKey}`;
-        const response = await httpGet(url);
-        return response.data[0];
-    } catch (error) {
-        console.error('Error fetching validator info:', error);
-        throw error;
-    }
-}
+        const body = JSON.stringify({ indices: pubKeys });
 
-async function getBlockAttestations(slot) {
-    try {
-        const url = `${lodestarApiUrl}/beacon/blocks/${slot}/attestations`;
-        const response = await httpGet(url);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching block attestations:', error);
-        throw error;
-    }
-}
-
-async function getLastAttestation(pubKey) {
-    try {
-        const validatorInfo = await getValidatorInfo(pubKey);
-        console.log(validatorInfo);
-       
-        const lastAttestationSlot = 16588429/*validatorInfo.index*/;
-
-        const attestations = await getBlockAttestations(lastAttestationSlot);
-        console.log(attestations);
-        /*const validatorAttestation = attestations.find(attestation =>
-            attestation.data.attesting_indices.includes(validatorInfo.validator.index)
-        );*/
-
-        return {
-            pubKey/*,
-            lastAttestation: validatorAttestation ? validatorAttestation.data : null*/
+        const options = {
+            hostname: 'localhost',
+            port: 9596,
+            path: '/eth/v1/validator/liveness',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': body.length
+            }
         };
+
+        return new Promise((resolve, reject) => {
+            const req = http.request(options, (res) => {
+                let data = '';
+
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+
+                res.on('end', () => {
+                    resolve(JSON.parse(data));
+                });
+            });
+
+            req.on('error', (err) => {
+                reject(err);
+            });
+
+            req.write(body);
+            req.end();
+        });
     } catch (error) {
-        console.error('Error fetching last attestation:', error);
+        console.error('Error fetching validator liveness:', error);
         throw error;
     }
 }
@@ -170,9 +137,17 @@ async function getLastAttestation(pubKey) {
 (async () => {
     const pubKeys = [
         '0xa1d1ad0714035353258038e964ae9675dc0252ee22cea896825c01458e1807bfad2f9969338798548d9858a571f7425c'
-        // Next pubkey
+        // Přidejte další pub keys podle potřeby
     ];
 
-    const validatorAttestations = await Promise.all(pubKeys.map(getLastAttestation));
-    console.log(validatorAttestations);
+    const validatorLiveness = await getValidatorLiveness(pubKeys);
+    console.log(validatorLiveness);
 })();
+
+function cleanUpAndExit() {
+    console.log("exiting");
+    // exit services
+    process.exit(0);
+}
+process.on('SIGTERM', cleanUpAndExit);
+process.on('SIGINT', cleanUpAndExit);
