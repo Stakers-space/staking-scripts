8e783f5ba5b02bb3cab3879046b159e81699cdbc
it4world <itgg2@proton.me>
2024-10-12 14:36:11 +0200

    Sending percentage values only

--- monitor/hw_sources/hw_usage_monitor.sh
+++ monitor/hw_sources/hw_usage_monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -r version="1.0.0"
+declare -r version="1.0.1"
 get_version() {
     echo -e "HW usage monitor version: $version | Powered by https://stakers.space"
     exit 0
@@ -13,8 +13,21 @@ server_id=$3
 disk_usage=$(df -h --output=pcent / | tail -n 1 | tr -d '[:space:]')
 
 # Get RAM and SWAP memmmory usage
-ram_usage=$(free -h | awk '/Mem:/ {print $3 "/" $2}')
-swap_usage=$(free -h | awk '/Swap:/ {print $3 "/" $2}')
+# ram_usage=$(free -h | awk '/Mem:/ {print $3 "/" $2}')
+ram_total=$(free | awk '/Mem:/ {print $2}')
+ram_used=$(free | awk '/Mem:/ {print $3}')
+ram_usage=$(awk "BEGIN {printf \"%.2f\", ($ram_used/$ram_total)*100}")
+
+#swap_usage=$(free -h | awk '/Swap:/ {print $3 "/" $2}')
+swap_total=$(free | awk '/Swap:/ {print $2}')
+swap_used=$(free | awk '/Swap:/ {print $3}')
+swap_usage=$(awk "BEGIN {printf \"%.2f\", ($swap_used/$swap_total)*100}")
+
+# CPU temp (requires sensors) || sudo apt install lm-sensors → sudo sensors-detect → sensors | grep 'Core'
+#cpu_temp=$(sensors | grep 'Core 0' | awk '{print $3}' | tr -d '+°C')
+
+# NVMe temp (requires nvme-cli) || sudo apt install nvme-cli → sudo nvme smart-log /dev/nvme0 | grep 'temperature'
+nvme_temp=$(sudo nvme smart-log /dev/nvme0 | grep 'temperature' | awk '{print $3}')
 
 # Post data through GET request
 server_url="https://stakers.space/api/hw-report"
