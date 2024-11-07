3fc765d7b9fb07bb7814b616679d3769e38dcd62
it4world <itgg2@proton.me>
2024-11-07 11:49:26 +0100

    Remove Lock files for Teku

---
+++ teku/remove_lock_files/README.md
@@ -0,0 +1,18 @@
+# Utility shell scripts for removing .lock files
+
+- Download the script to `/usr/local/bin` directory
+```
+sudo curl -o /usr/local/bin/remove-lock-files.sh https://raw.githubusercontent.com/Stakers-space/staking-scripts/main/teku/remove_lock_files/remove-lock-files.sh
+```
+- Enable execution of the shell script
+```
+sudo chmod +x /usr/local/bin/remove-lock-files.sh
+```
+- Use the script
+```
+/usr/local/bin/remove-lock-files.sh <refferent lock file in directorz in which all .lock file should be deleted>
+```
+Sample:
+```
+/usr/local/bin/remove-lock-files.sh '/var/lib/teku-validator/validator_keys/keystore-m_12381_3600_0_0_0-1710000000000.lock'
+```
---
+++ teku/remove_lock_files/remove-lock-files.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+referrentLockFile=$1
+
+## check instance definition
+if [ -z "$referrentLockFile" ]; then
+    echo "missing referrentLockFile parameter: $0 | Sample: '/var/lib/teku-validator/validator_keys/keystore-m_12381_3600_0_0_0-1710000000000.lock'"
+    exit 1
+fi
+
+## Check, whether $referrentLockFile exists
+if [ -f "$referrentLockFile" ]; then
+    echo "✔ Referrent file $referrentLockFile found."
+fi
+
+# Get the directory of the keystore file (e.g. /var/lib/teku-validator/validator_keys/)
+directory_path=$(dirname "$referrentLockFile")
+files=$(ls "$directory_path")
+num_files=$(echo "$files" | wc -l)
+echo "Referrent file directory is: $directory_path | Files in the folder: $num_files"
+
+# Iterate over files in the folder and delete any .lock files
+for f in $files; do
+    if [[ $f == *.lock ]]; then
+        echo "Deleting $f"
+        rm "$directory_path/$f"
+        echo "File $f was deleted."
+    fi
+done
